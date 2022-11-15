ed95f8d430b4846d30db1842f6410fd79f4646ee
Benjamín Olvera Rosales <benjaminolverarosales@gmail.com>
2022-11-15 13:32:08 -0600

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,12 +5,17 @@ namespace App\Exceptions;
 use Throwable;
 use App\Enums\HttpStatusEnum;
 use App\Helpers\ResponseHelper;
+use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -61,7 +66,6 @@ class Handler extends ExceptionHandler
      */
     public function handleException(Throwable $th)
     {
-        $status    = $this->isHttpException($th) ? $th->getStatusCode() : 500;
         $message   = $th->getMessage();
         $data      = [];
 
@@ -73,10 +77,18 @@ class Handler extends ExceptionHandler
             ];
         }
 
+        /*
+         * ****************************************
+         *   Most common errors handling
+         * ****************************************
+         */
+
         if ($th instanceof ValidationException) {
             $status  = HttpStatusEnum::UNPROCESSABLE_ENTITY;
             $message = 'Los datos recibidos son incorrectos';
             $data    = $th->validator->errors()->getMessages();
+
+            return ResponseHelper::jsonError($message, $data, $status);
         }
 
         if ($th->getPrevious() instanceof ModelNotFoundException) {
@@ -88,6 +100,8 @@ class Handler extends ExceptionHandler
                 $id    = $th->getIds()[0];
                 $message = "No hay resultados para el modelo $model $id";
             }
+
+            return ResponseHelper::jsonError($message, $data, $status);
         }
 
         if ($th instanceof AuthenticationException) {
@@ -96,6 +110,7 @@ class Handler extends ExceptionHandler
                 $message = 'No autorizado';
             }
 
+            return ResponseHelper::jsonError($message, $data, $status);
         }
 
         if ($th instanceof AuthorizationException) {
@@ -103,6 +118,8 @@ class Handler extends ExceptionHandler
             if (trim($message) === '') {
                 $message = 'Acción prohibida';
             }
+
+            return ResponseHelper::jsonError($message, $data, $status);
         }
 
         if ($th instanceof NotFoundHttpException) {
@@ -110,6 +127,62 @@ class Handler extends ExceptionHandler
             if (trim($message) === '') {
                 $message = 'No se encontró';
             }
+
+            return ResponseHelper::jsonError($message, $data, $status);
+        }
+
+        if ($th instanceof MethodNotAllowedHttpException) {
+            $status  = HttpStatusEnum::METHOD_NOT_ALLOWED;
+            if (trim($message) === '') {
+                $message = 'Método no permitido';
+            }
+
+            return ResponseHelper::jsonError($message, $data, $status);
+        }
+
+        if ($th instanceof UnprocessableEntityHttpException) {
+            $status  = HttpStatusEnum::UNPROCESSABLE_ENTITY;
+            $data = json_decode($th->getMessage(), true) ?? $th->getMessage();
+            $message = $data['in'][0] ?? 'Los datos recibidos son incorrectos.';
+
+            return ResponseHelper::jsonError($message, $data, $status);
+        }
+
+        if ($th instanceof HttpException) {
+            $status  = $th->getStatusCode();
+            if (trim($message) === '') {
+                $message = 'Error';
+            }
+
+            return ResponseHelper::jsonError($message, $data, $status);
+        }
+
+        if ($th instanceof ThrottleRequestsException) {
+            $status  = HttpStatusEnum::TOO_MANY_REQUESTS;
+            $message = 'Demasiadas solicitudes';
+
+            return ResponseHelper::jsonError($message, $data, $status);
+        }
+
+        if ($th instanceof QueryException) {
+            $message = $th->getMessage();
+            $code    = $th->errorInfo[1] == 2002 ? 500 : 409;
+
+            return ResponseHelper::jsonError($message, $data, $code);
+        }
+
+        /*
+         * ****************************************
+         *  End Most common errors handling
+         * ****************************************
+         */
+
+        $status = $this->isHttpException($th) ? $th->getStatusCode() : HttpStatusEnum::INTERNAL_SERVER_ERROR;
+        $message = self::GLOBAL_ERROR_MESSAGE;
+
+        if (!config('app.debug')) {
+            $message = trim($th->getMessage()) === '' ? $message : $th->getMessage();
+            $data    = $dataDev;
         }
 
         return ResponseHelper::jsonError($message, $data, $status);
